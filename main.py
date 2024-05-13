7ad484b6ec1eeb7e2e493f158db254e15ca14e26
jesusgtxl <jesusgtxl@gmail.com>
2024-05-13 08:54:39 -0600

    main y módulo creados

---
+++ main.py
@@ -0,0 +1,5 @@
+from miModulo import Sensor
+
+if __name__ == "__main__":
+    obj=Sensor()
+    print(obj.pin)
---
+++ miModulo.py
@@ -0,0 +1,6 @@
+class Sensor:
+    def __init__(self):
+        self.pin=15
+    def inicializar(self,tipo):
+        self.tipo=tipo
+
